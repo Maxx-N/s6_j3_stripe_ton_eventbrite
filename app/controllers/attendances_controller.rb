5a75359ddc77b41fc092f422f79468a020f6115a
Maxx-N <maximenorval@gmail.com>
2019-11-08 11:01:25 +0100

    Inscription et désinscription à un évènement (avec alertes)

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,10 +6,21 @@ class AttendancesController < ApplicationController
     unless a.event.users.include?(a.user)
       a.save
       redirect_to event_path(params[:my_event_id])
-      flash[:you_attend] = "Bienvenu(e) #{current_user.first_name} ! Tu viens de t'inscrire à #{a.event.title} ! :D"
+      if current_user.first_name
+        flash[:you_attend] = "Bienvenu(e) #{current_user.first_name} ! Tu viens de t'inscrire à #{a.event.title} ! :D"
+      else
+        flash[:you_attend] = "Bienvenu(e) #{current_user.email} ! Tu viens de t'inscrire à #{a.event.title} ! :D"
+      end
     else
       redirect_to events_path
       flash[:already_attending] = "Tu participes déjà à cet évènement ;)"
     end
+  end
+
+  def destroy
+    a = Attendance.find(params[:id])
+    a.destroy
+    redirect_to events_path
+    flash[:unattending] = "Tu t'es bien désinscrit(e) de l'évènement #{a.event.title}"
   end
 end
