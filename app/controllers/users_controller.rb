a6c7f3843aee6919214c6f3ec8ad8c5b127c4f62
Maxx-N <maximenorval@gmail.com>
2019-11-08 15:29:39 +0100

    accès réservé au profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  include UsersHelper
   before_action :authenticate_user!, only: [:show]
 
   def show
-    @my_user = User.find(params[:id])
+    if is_current_user?(User.find(params[:id]))
+      @my_user = User.find(params[:id])
+    else
+      flash[:not_your_profile] = "Tu ne peux pas accéder aux informations d'un autre utilisateur ... !"
+      redirect_to root_path
+    end
   end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+  def is_current_user?(user)
+    user == current_user
+  end
+
 end
